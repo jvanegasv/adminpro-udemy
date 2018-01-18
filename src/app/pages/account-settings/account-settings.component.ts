f4ee6bfc60633eed8956511bf8f6d96e91c24fee
Jorge Vanegas <jvanegasv@gmail.com>
2018-01-18 14:31:00 -0600

    Clase 59 ok

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -13,10 +13,23 @@ export class AccountSettingsComponent implements OnInit {
   ngOnInit() {
   }
 
-  cambiarColor(tema: string) {
+  cambiarColor(tema: string, link: any) {
 
-    let url = `assets/css/colors/${ tema }.css`;
+    this.aplicarCheck(link);
+
+    const url = `assets/css/colors/${ tema }.css`;
     this._document.getElementById('tema').setAttribute('href', url);
+  }
+
+  aplicarCheck(link: any) {
+
+    const selectores: any = document.getElementsByClassName('selector');
+
+    for (const ref of selectores) {
+      ref.classList.remove('working');
+    }
+
+    link.classList.add('working');
   }
 
 }
