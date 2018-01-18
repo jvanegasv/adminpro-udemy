241c6eb58622ac98595e0c648c00d901ed954276
Jorge Vanegas <jvanegasv@gmail.com>
2018-01-18 11:29:56 -0600

    clase 45 ok

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncrementadorComponent implements OnInit {
 
-  leyenda: string = 'Leyenda';
-  progreso: number = 50;
+  @Input('nombre') leyenda: string = 'Leyenda';
+  @Input() progreso: number = 50;
 
   constructor() { }
 
