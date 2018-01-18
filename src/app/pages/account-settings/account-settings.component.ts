10792c5c18ea85ea8df7b95a33e2d8308d2227ef
Jorge Vanegas <jvanegasv@gmail.com>
2018-01-18 14:20:14 -0600

    Clase 58 ok

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-account-settings',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor( @Inject(DOCUMENT) private _document) { }
 
   ngOnInit() {
+  }
+
+  cambiarColor(tema: string) {
+
+    let url = `assets/css/colors/${ tema }.css`;
+    this._document.getElementById('tema').setAttribute('href', url);
   }
 
 }
