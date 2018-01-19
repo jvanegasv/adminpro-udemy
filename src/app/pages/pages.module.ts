0080b28078fcd6bdaee2e89938d2f2c61b4eb46a
Jorge Vanegas <jvanegasv@gmail.com>
2018-01-19 09:52:38 -0600

    Clase 72 ok

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -13,6 +13,7 @@ import {PagesComponent} from './pages.component';
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
 
 // ng2-charts
 import { ChartsModule } from 'ng2-charts';
@@ -25,7 +26,8 @@ import { ChartsModule } from 'ng2-charts';
     PagesComponent,
     IncrementadorComponent,
     GraficoDonaComponent,
-    AccountSettingsComponent
+    AccountSettingsComponent,
+    PromesasComponent
   ],
   exports: [
     DashboardComponent,
