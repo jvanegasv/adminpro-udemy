60ec60120ee2964281ce22352eb838d73d2bb36d
Jorge Vanegas <jvanegasv@gmail.com>
2018-01-19 10:21:24 -0600

    Clase 74 ok

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// tslint:disable-next-line:import-blacklist
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-rxjs',
@@ -7,7 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RxjsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    const obs = new Observable(
+      observer => {
+
+        let contador = 0;
+        const intervalo = setInterval( () => {
+
+          contador += 1;
+          observer.next(contador);
+          if (contador === 3) {
+            clearInterval(intervalo);
+            observer.complete();
+          }
+
+          if (contador === 2) {
+            observer.error('Auxilio...');
+          }
+
+        }, 1000);
+      }
+    );
+
+    obs.subscribe(
+      numero => console.log('Sub: ', numero),
+      error => console.error('Error: ', error),
+      () => console.log('Fin del observador')
+    );
+
+  }
 
   ngOnInit() {
   }
