0edfd1cb0f0a7f76cc87e2b51ca3652684dbbd25
Jorge Vanegas <jvanegasv@gmail.com>
2018-01-26 12:48:13 -0600

    Clase 158 ok

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,8 +3,12 @@ import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { UsuarioService } from '../services/service.index';
 import { Usuario } from '../models/usuario.model';
+import { element } from 'protractor';
 
 declare function init_plugins();
+
+// cargar libreria de google
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -15,15 +19,47 @@ export class LoginComponent implements OnInit {
 
   email: string;
   recuerdame: boolean = false;
+
+  auth2: any;
+
   constructor(public router: Router, public _usuarioService: UsuarioService) { }
 
   ngOnInit() {
     init_plugins();
 
+    this.googleInit();
+
     this.email = localStorage.getItem('email') || '';
     if (this.email.length > 1) {
       this.recuerdame = true;
     }
+  }
+
+  googleInit() {
+
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id : '723672426611-3o9sgungqmubg0bftfnim5nelsg3mfbe.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+
+      this.attachSignin(document.getElementById('btnGoogle'));
+
+    });
+  }
+
+  attachSignin( element ) {
+
+    this.auth2.attachClickHandler(element, {}, (googlUser) => {
+
+      const profile = googlUser.getBasicProfile();
+      const token = googlUser.getAuthResponse().id_token;
+
+      console.log(profile);
+      console.log(token);
+
+    });
   }
 
   ingresar(forma: NgForm) {
