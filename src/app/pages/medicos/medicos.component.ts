450133b8ca14a6e0e1d3ca122bf6c48ad271cd33
Jorge Vanegas <jvanegasv@gmail.com>
2018-02-02 10:29:59 -0600

    clase 199 ok

--- src/app/pages/medicos/medicos.component.ts
+++ src/app/pages/medicos/medicos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MedicoService } from '../../services/service.index';
+import { Medico } from '../../models/medico.model';
 
 @Component({
   selector: 'app-medicos',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicosComponent implements OnInit {
 
-  constructor() { }
+  medicos: Medico[] = [];
+
+  constructor(
+    public _medicosService: MedicoService
+  ) { }
 
   ngOnInit() {
+
+    this.cargarMedicos();
   }
 
+  cargarMedicos() {
+
+    this._medicosService.cargarMedicos().subscribe( medicos => this.medicos = medicos);
+
+  }
+
+  buscarMedico() {
+
+  }
+
+  crearMedico() {
+
+  }
+
+  editarMedico() {
+
+  }
+
+  borrarMedico() {
+    
+  }
 }
--- src/app/services/medico/medico.service.ts
+++ src/app/services/medico/medico.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../../config/config';
 
 @Injectable()
 export class MedicoService {
 
-  constructor() { }
+  public totalMedicos: number = 0;
+
+  constructor(
+    public http: HttpClient
+  ) { }
+
+  cargarMedicos() {
+
+    const url = URL_SERVICIOS + '/medico';
+
+    return this.http.get(url).map( (resp: any) => {
+
+      this.totalMedicos = resp.total;
+      return resp.medicos;
+    });
+  }
 
 }
